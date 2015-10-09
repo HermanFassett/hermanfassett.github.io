3311d06f0b23c79d6e1564f7417b6421a6e53f84
HermanFassett <hermanfassett@gmail.com>
2015-10-09 10:33:32 -0700

    Keyevents

--- jscalculator/js/index.js
+++ jscalculator/js/index.js
@@ -28,11 +28,45 @@ $(document).ready(function(){
       case 97:
         $("#btn1").click();
         break;
+      // 2
+      case 50:
+      case 98:
+        $("#btn2").click();
+        break;
+      case 51:
+      case 99:
+        $("#btn3").click();
+        break;
+      case 52:
+      case 100:
+        $("#btn4").click();
+        break;
+      case 53:
+      case 101:
+        $("#btn5").click();
+        break;
+      case 54:
+      case 102:
+        $("#btn6").click();
+        break;
+      case 55:
+      case 103:
+        $("#btn7").click();
+        break;
+      case 56:
+      case 104:
+        $("#btn8").click();
+        break;
+      case 57:
+      case 105:
+        $("#btn9").click();
+        break;
       // Enter key
       case 13:
         $("#btnEqual").click();
         break;
-      // C key
+      // C and Numpad clear keys
+      case 46:
       case 67:
         $("#btnC").click();
         break;

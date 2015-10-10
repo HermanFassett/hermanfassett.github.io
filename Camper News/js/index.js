44affd67bc6e8882b4bbf0149b9465bdf8064d75
HermanFassett <hermanfassett@gmail.com>
2015-10-09 18:58:45 -0700

    Final

--- Camper News/js/index.js
+++ Camper News/js/index.js
@@ -2,21 +2,29 @@ var MIN = 0;
 var MAX = 20;
 $(document).ready(function(){
   load();
+  $("#load").click(function() {
+    MIN += 20;
+    MAX += 20;
+    load();
+  });
 });
 function load() {
   $.getJSON("http://www.freecodecamp.com/news/hot", function(data) {
-    for (var i = MIN; i < MAX; i++) {
-      var link = data[i].link, picture = data[i].author.picture;
-      var headline = data[i].headline;
-      var author = data[i].author.username;
-      var likes = data[i].upVotes.length;
-      insert(link, picture, headline, author, likes);
+    if (MIN < 100) {
+      for (var i = MIN; i < MAX; i++) {
+        var link = data[i].link, picture = data[i].author.picture;
+        var headline = data[i].headline;
+        var author = data[i].author.username;
+        var likes = data[i].upVotes.length;
+        insert(link, picture, headline, author, likes);
+      }
     }
   });
   function insert(link, img, headline, author, likes) {
     headline = headline.substr(0, 20) + "...";
     var elem =
     "<div class='news-box'>" +
+      "<div class='likes'>+" + likes + "</div>" +
       "<a href='" + link + "'><img src='" + img +"'></a>" +
       "<div class='news-info'>" +
         "<a class='title' href='" + link + "'><p>" + headline + "</p></a>" +
@@ -26,8 +34,3 @@ function load() {
     $("#main").append(elem + " ");
   }
 }
-$("#load").click(function() {
-  MIN += 20;
-  MAX += 20;
-  load();
-});

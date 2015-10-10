ba11f1a5d9e5beae95fa9d16420dca9c45678490
HermanFassett <hermanfassett@gmail.com>
2015-10-09 18:02:20 -0700

    News

---
+++ Camper News/js/index.js
@@ -0,0 +1,33 @@
+var MIN = 0;
+var MAX = 20;
+$(document).ready(function(){
+  load();
+});
+function load() {
+  $.getJSON("http://www.freecodecamp.com/news/hot", function(data) {
+    for (var i = MIN; i < MAX; i++) {
+      var link = data[i].link, picture = data[i].author.picture;
+      var headline = data[i].headline;
+      var author = data[i].author.username;
+      var likes = data[i].upVotes.length;
+      insert(link, picture, headline, author, likes);
+    }
+  });
+  function insert(link, img, headline, author, likes) {
+    headline = headline.substr(0, 20) + "...";
+    var elem =
+    "<div class='news-box'>" +
+      "<a href='" + link + "'><img src='" + img +"'></a>" +
+      "<div class='news-info'>" +
+        "<a class='title' href='" + link + "'><p>" + headline + "</p></a>" +
+        "<a class='author' href='http://freecodecamp.com/" + author + "'>by " + author + "</a>" +
+      "</div" +
+    "</div>";
+    $("#main").append(elem + " ");
+  }
+}
+$("#load").click(function() {
+  MIN += 20;
+  MAX += 20;
+  load();
+});

a701796387195950f8fb359faeec2a25c48bdbaa
HermanFassett <hermanfassett@gmail.com>
2015-10-30 22:54:08 -0700

    Update

---
+++ apps/random-quotes/js/index.js
@@ -0,0 +1,24 @@
+$("document").ready(function() {
+  random();
+});
+$("#btn-new").click(function() {
+  random();
+});
+var random = function() {
+  $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(data) {
+    $("#quote").text(data.quoteText);
+    $("#author").text(data.quoteAuthor);
+    tweetUpdate(data.quoteText, data.quoteAuthor);
+  });
+}
+var tweetUpdate = function(quote, author) {
+  if (author != "")
+    author = " - " + author;
+  $("#tweet-content").empty();
+ twttr.widgets.createHashtagButton(window.location.href,
+    document.getElementById("tweet-content"), {
+      text: ("\"" + quote + "\"" + author).substr(0, 126),
+      size: "large"
+    }
+  );
+}

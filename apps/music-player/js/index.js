18e70af51c3e9228cf14d6afe5011a5c554a5eda
HermanFassett <hermanfassett@gmail.com>
2015-11-04 22:05:51 -0800

    Music player
    Testing a new app

---
+++ apps/music-player/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  function load() {
+    $.ajax({
+      url: "../audio",
+      success: function (data) {
+        console.log(data);
+      }
+    });
+  }
+  function resize() {
+    $(".list").css("height", ($(window).height() - 25) + "px");
+    $(".list-item").css("width", ($(".list-item").parent().width()/2 - 10) + "px");
+  }
+  $(window).on("resize" ,function() {resize()});
+  load();
+  resize();
+});

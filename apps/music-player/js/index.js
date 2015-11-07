6743df5774d349e6249d597b5f649341bc78d684
HermanFassett <hermanfassett@gmail.com>
2015-11-07 09:37:54 -0800

    Edit

--- apps/music-player/js/index.js
+++ apps/music-player/js/index.js
@@ -1,12 +1,30 @@
 $(document).ready(function() {
+  var aud = ["A Winter Day", "Brightness Davar", "Exiled",
+  "Sunrise", "The Path To Freedom"];
+  var audio;
   function load() {
-    $.ajax({
-      url: "./audio",
-      success: function (data) {
-        console.log(data);
-      }
-    });
+    for (var a in aud) {
+      var song = aud[a];
+      $(".list").append("<div class='list-item'><h1>" +
+      song + "</h1></div>");
+    }
   }
+  $(document).on("click", ".list-item", function() {
+    var ctx = new AudioContext();
+    audio = new Audio("./audio/" + $(this).text() + ".mp3");
+    var audioSrc = ctx.createMediaElementSource(audio);
+    var analyser = ctx.createAnalyser();
+    audioSrc.connect(analyser);
+    $(".player").css("display", "block");
+
+    audio.play();
+  });
+  $(".ball").click(function() {
+    (audio.paused) ? audio.play() : audio.pause();
+  });
+  $(".ball").hover(function() {
+
+  });
   function resize() {
     $(".list").css("height", ($(window).height() - 25) + "px");
     $(".list-item").css("width", ($(".list-item").parent().width()/2 - 10) + "px");

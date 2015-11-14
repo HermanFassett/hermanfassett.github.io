cf84586a1461db23e79b45adde347099f19b1782
HermanFassett <hermanfassett@gmail.com>
2015-11-13 20:34:05 -0800

    Update
    Added new images, new music piece, descriptions for content, and
    comments

--- js/script.js
+++ js/script.js
@@ -1,40 +1,50 @@
 (function($) {
-  var small = false;
-$(document).ready(function(){
-  $(window).scroll( function(){
-    $(".fadein").each( function(){
-      var object_top = $(this).position().top;
-      var screen_bottom = $(window).scrollTop() + $(window).height();
-      if(screen_bottom > object_top)
-        $(this).animate({'opacity':'1'},1500);
+  var small = false; //Boolean width < 1000
+  /* On Document Ready */
+  $(document).ready(function() {
+    /* On Window Scroll */
+    $(window).scroll(function() {
+      // For each element with class fadein
+      $(".fadein").each(function() {
+        var object_top = $(this).position().top; // Top of current element
+        var screen_bottom = $(window).scrollTop() + $(window).height(); //Bottom of visible screen
+        if(screen_bottom > object_top)
+          $(this).animate({'opacity':'1'},1500); // Show element if in view
+      });
+      // If the width is 1000 pixels or greater
+      if (!small) {
+        var nav = $(".menu")[0]; // Get the menu bar
+        var circle = $(".circle")[1]; // Get the main circle
+        var bottom = $(circle).position().top + $(circle).outerHeight(); // Get bottom of circle
+        if (bottom < $(window).scrollTop())
+          $(nav).animate({"opacity":"1"}, 100); // Show menu if main circle isn't in view
+        else $(nav).animate({"opacity":"0"}, 100); // Otherwise hide menu bar
+      }
     });
-    if (!small) {
-      var nav = $(".menu")[0];
-      var circle = $(".circle")[1];
-      var bottom = $(circle).position().top + $(circle).outerHeight();
-      if (bottom < $(window).scrollTop()) $(nav).animate({"opacity":"1"}, 100);
-      else $(nav).animate({"opacity":"0"}, 100);
+
+    /* On resize */
+    $(window).resize(function() {
+      resize(); // Call resize function
+    });
+
+    /* Sizing function */
+    function resize() {
+      // If window width is less than 1000
+      if ($(window).width() < 1000) {
+        small = true; // Set boolean small screen
+        $(".background-img").addClass("small"); // Set background image small
+        $(".main").hide(); // Hide main div
+        $(".circle").hide(); // Hide circles
+        $($(".menu")[0]).addClass("largemenu"); // Show menu permanently
+      } // Otherwise...
+      else {
+        small = false; // Set boolean small screen false
+        $(".main").show(); // Show main div
+        $(".circle").show(); // Show circles
+        $($(".menu")[0]).removeClass("largemenu"); // Set menu view back to normal
+        $(".background-img").removeClass("small"); // Reset background image size
+      }
     }
+    resize(); // Call resize function on reload
   });
-  $(window).resize(function() {
-    resize();
-  });
-  function resize() {
-    if ($(window).width() < 1000) {
-      small = true;
-      $(".background-img").addClass("small");
-      $(".main").hide();
-      $(".circle").hide();
-      $($(".menu")[0]).addClass("largemenu");
-    }
-    else {
-      small = false;
-      $(".main").show();
-      $(".circle").show();
-      $($(".menu")[0]).removeClass("largemenu");
-      $(".background-img").removeClass("small");
-    }
-  }
-  resize();
-});
 })(jQuery)

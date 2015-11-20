e602a46afb984ebd79ad576a7a49bdb0ec1ab850
HermanFassett <hermanfassett@gmail.com>
2015-11-20 11:45:35 -0800

    Update
    Fixing ai to be more intelligent though not yet beatable

--- apps/tic-tac-toe/js/index.js
+++ apps/tic-tac-toe/js/index.js
@@ -1,84 +1,113 @@
+var arr = "000"+
+          "000"+
+          "000",
+          user = "X", ai = "O";
+var rows = function() {
+    return arr.match(/.{1,3}/g);
+};
+var columns = function() {
+    return [arr[0]+arr[3]+arr[6],arr[1]+arr[4]+arr[7],arr[2]+arr[5]+arr[8]];
+};
+var diagonals = function() {
+    return [arr[0]+arr[4]+arr[8], arr[2]+arr[4]+arr[6]];
+};
+var indexOf = function(type, a) {
+    var index = a[0], offset = a[1];
+    if (type === "r") return (offset * 3) + index;
+    else if (type === "c") return (index * 3) + offset;
+    else if (type === "d") return (offset === 0) ? index * 4 : index * 2 + 2;
+};
+var wins = function() {
+    var rs = rows(), cs = columns(), ds = diagonals();
+    var winner = false;
+    rs.forEach(check);
+    cs.forEach(check);
+    ds.forEach(check);
+    function check(a, index) {
+        if (a.match(/X{3}|O{3}/)) winner = true;
+    }
+    return winner;
+};
+var nextWins = function() {
+    var rs = rows(), cs = columns(), ds = diagonals(), type = "r";
+    var wins = [];
+    rs.forEach(check);
+    type = "c";
+    cs.forEach(check);
+    type = "d";
+    ds.forEach(check);
+    function check(a, index) {
+        var b = a.match(/X{2}|O{2}/);
+        if (b) wins.push(indexOf(type, [(b.index === 0) ? 2 : 0, index]));
+        else {
+            b = a.match(/X0X|O0O/);
+            if (b) wins.push(indexOf(type, [1, index]));
+        }
+    }
+    return wins;
+};
+var possibleMoves = function() {
+  var moves = [];
+    arr.split("").forEach(function(a, index) {
+      if (a === "0") moves.push(index);
+    });
+    return moves;
+};
 $(document).ready(function() {
   var turn = true;
-  var c = [["", "", ""],["","",""],["","",""]];
-  var cs = [[$("#c1"), $("#c2"), $("#c3")],
-            [$("#c4"), $("#c5"), $("#c6")],
-            [$("#c7"), $("#c8"), $("#c9")]];
-  $("#c1, #c2, #c3, #c4, #c5, #c6, #c7, #c8, #c9").click(function() {
+  var cs = [$("#c1"),$("#c2"),$("#c3"),$("#c4"), $("#c5"), $("#c6"),$("#c7"),$("#c8"),$("#c9")];
+  $(".box").click(function() {
     if (turn) {
       for (var i = 0; i < cs.length; i++) {
-        for (var j = 0; j < cs[i].length; j++) {
-          if (this.id === cs[i][j][0].id && c[i][j] === "") {
-            $("#text").css("display", "none");
-            $(cs[i][j]).css("background-image", "url('images/X.png')");
-            c[i][j] = "X";
-            check();
-            go();
-            break;
-          }
+        if (this.id === cs[i][0].id && arr[i] === "0") {
+          $("#text").css("display", "none");
+          $(cs[i]).css("background-image", "url('images/" + user + ".png')");
+          arr = arr.substr(0,i) + user + arr.substr(i+1);
+          go();
+          break;
         }
       }
     }
   });
-  function check() {
+  function go() {
     turn = false;
-    // Diagonal
-    if (c[1][1] !== "" && ((c[0][0] === c[1][1] && c[1][1] === c[2][2]) || (c[0][2] === c[1][1] && c[1][1] === c[2][0]))) {
-        win(c[0][0]);
-      }
-    // Horizontal
-    for (var i = 0; i < c.length; i++) {
-      if (c[i][0] !== "" && c[i][0] === c[i][1] && c[i][1] === c[i][2]) {
-        win(c[i][0]);
-        break;
-      }
+    if (wins()) win(user);
+    else if (nextWins().length > 0) {
+      var moves = nextWins();
+      var best = [];
+      moves.forEach(function(a) {
+        if (arr[a] === "0") best.push(a);
+      });
+      best.forEach(function(b,i) {
+        var temp = arr;
+        arr = arr.substr(0, b) + ai + arr.substr(b + 1);
+        if (wins()) best = [b];
+        arr = temp;
+      });
+      arr = arr.substr(0, best[0]) + ai + arr.substr(best[0] + 1);
+      $(cs[best[0]]).css("background-image", "url('images/" + ai +".png')");
+      if (wins()) win(ai);
     }
-    // Vertical
-    for (var j = 0; j < 3; j++) {
-      if (c[0][j] !== "" && c[0][j] === c[1][j] && c[1][j] === c[2][j]) {
-        win(c[0][j]);
-        break;
-      }
+    else if (possibleMoves().length > 0) {
+      var moves = possibleMoves();
+      arr = arr.substr(0, moves[0]) + ai + arr.substr(moves[0] + 1);
+      $(cs[moves[0]]).css("background-image", "url('images/" + ai + ".png')");
     }
+    if (possibleMoves().length === 0 && !wins()) win("tie");
+    turn = true;
   }
   function win(ty) {
     $("#text").css("display", "block");
     $(".box").css("pointer-events", "none");
-    (ty === "tie") ? $("#text").text("Cat's Game!") : $("#text").text(ty + " wins!");
+    $("#text").text((ty === "tie") ? "Cat's Game!": ty + " wins!");
     setTimeout(start, 3000);
-  }
-  function go() {
-    turn = false;
-    var is = [];
-    var js = [];
-    for (var i = 0; i < c.length; i++) {
-      for (var j = 0; j < c.length; j++) {
-        if (c[i][j] === "") {
-          is.push(i);
-          js.push(j);
-        }
-      }
-    }
-    if (!is.length) {
-      win("tie");
-    }
-    else {
-      var x = Math.floor(Math.random() * is.length);
-      c[is[x]][js[x]] = "O";
-      $(cs[is[x]][js[x]]).css("background-image", "url('images/O.png')");
-    }
-    check();
-    turn = true;
   }
   function start() {
     $(".box").css("pointer-events", "auto");
     $("#text").css("display", "none");
-    c = [["", "", ""],["","",""],["","",""]];
+    arr = "000000000";
     for (var i = 0; i < cs.length; i++) {
-      for (var j = 0; j < cs[i].length; j++) {
-        $(cs[i][j]).css("background-image", "none");
-        $(cs[i][j]).css("background-size", "100%, 100%;");
-      }
+      $(cs[i]).css("background", "white");
     }
   }
 });
